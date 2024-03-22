21197130c8dff1174bc062a2cd6e43dcc245bed4
limeaicell <celltesting@lime.ai>
2024-03-22 12:02:08 +0530

    Fixed init file Signed-off-by: limeaicell <celltesting@lime.ai>

--- src/LimeNDAX/__init__.py
+++ src/LimeNDAX/__init__.py
@@ -1,11 +1,10 @@
 # __init__.py
 
-from .ndax_functions_new import get_process_name
-from .ndax_functions_new import get_barcode
-from .ndax_functions_new import get_starttime
-from .ndax_functions_new import get_remarks
-from .ndax_functions_new import get_cycle
-from .ndax_functions_new import get_recipe
-from .ndax_functions_new import get_step
-from .ndax_functions_new import get_records
-
+from .ndax_functions import get_process_name
+from .ndax_functions import get_barcode
+from .ndax_functions import get_starttime
+from .ndax_functions import get_remarks
+from .ndax_functions import get_cycle
+from .ndax_functions import get_recipe
+from .ndax_functions import get_step
+from .ndax_functions import get_records
